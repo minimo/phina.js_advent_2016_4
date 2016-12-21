d1eef491f6ca1080a6d337d0bc038341bc7cb804
minimo <minimo.mobile@gmail.com>
2016-12-21 12:23:21 +0900

    Add initial files.

---
+++ main.js
@@ -0,0 +1,55 @@
+/*
+
+　マルチタッチサンプル
+
+*/
+
+/*
+ * メインシーン
+ */
+phina.define('MainScene', {
+  superClass: 'phina.display.DisplayScene',
+
+  // 初期化
+  init: function() {
+    // super init
+    this.superInit();
+
+    // 背景色
+    this.backgroundColor = '#FFF';
+
+    this.label = phina.display.Label('Hello, runstant!').addChildTo(this);
+    this.label.x = this.gridX.center();
+    this.label.y = this.gridY.center();
+    this.label.update = function() {
+    }
+  },
+
+  update: function() {
+    var list = app.touchList;
+    if (list.touches.length != 0) {
+        this.label.text = "ok";
+    }
+  },
+
+  onkeydown: function(e) {
+    // space if push space
+    if (e.keyCode === 32) {
+      this.app.stop();
+    }
+  },
+});
+
+/*
+ * メイン処理
+ */
+phina.main(function() {
+  // アプリケーションを生成
+  app = phina.game.GameApp({
+    startLabel: 'main',
+  });
+  app.enableStats();
+
+  // 実行
+  app.run();
+});
